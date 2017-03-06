67fa9b5b466e4edfe829e93f7c96bae89a555f9c
Sofistication <sordonez93@gmail.com>
2017-03-06 14:17:56 -0500

    Fix filename and folder problem
    - files no longer save to the bucket root directory
    - create new folder named with today's date
    - used crypto.randomBytes to generate new pseudorandom filenames
    - refactored randomBytes and s3.upload to use promises

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -8,6 +8,7 @@ const s3 = new AWS.S3();
 
 const mime = require('mime');
 const path = require('path');
+const crypto = require('crypto');
 
 let file = {
   path: process.argv[2],
@@ -16,16 +17,39 @@ let file = {
 
 let mimeType = mime.lookup(file.path);
 let ext = path.extname(file.path);
+let folder = (new Date()).toISOString().split('T')[0];
 let stream = fs.createReadStream(file.path);
 
-let params = {
-  ACL: 'public-read',
-  Bucket: process.env.AWS_S3_BUCKET_NAME,
-  Key: `${file.title}${ext}`,
-  Body: stream,
-  ContentType: mimeType
-};
+new Promise((resolve, reject) => {
+  crypto.randomBytes(16, (err, buffer) => {
+    if (err) {
+      reject(err);
+    } else {
+      console.log('buffer is ', buffer);
+      console.log('buffer.toS is ', buffer.toString('hex'));
+      resolve(buffer.toString('hex'));
+    }
+  });
+})
+.then((filename) => {
+  let params = {
+    ACL: 'public-read',
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
+    Key: `${folder}/${filename}${ext}`,
+    Body: stream,
+    ContentType: mimeType
+  };
 
-s3.upload(params, function (err, data) {
-  console.log(err, data);
-});
+  return new Promise((resolve, reject) => {
+    s3.upload(params, function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+})
+.then(console.log)
+.catch(console.error)
+;
